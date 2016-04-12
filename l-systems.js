7509e2ec641fb1e795ff651c0f4c3b9032267d0c
Chandra Galbiati <chandra.galbiati@gmail.com>
2016-04-12 18:10:48 -0400

    fractals work

--- l-systems.js
+++ l-systems.js
@@ -30,34 +30,31 @@ function parseInstr(ctx, currState, dist=20, angle=25){
   let newInstr = currState.instr.slice(1);
   let newDir;
   let newStack;
-  console.log('currState', currState.instr[0],currState.instr.length, currState);
+  // console.log('currState', currState.instr[0],currState.instr.length, currState);
 
   switch(currState.instr[0]){
     case 'F':
     case 'G':
-      console.log('draw')
       let [newX, newY] = genLine(curX, curY, currState.curDir, dist);
+      // console.log('draw', curX, curY, newX, newY, currState.curDir);
       drawLine(ctx, curX, curY, newX, newY);
       return Object.assign({}, currState, {curPos: [newX, newY]}, {instr: newInstr});
     case '+':
-      console.log('+')
       newDir = currState.curDir + angle <= 360 ? currState.curDir + angle : currState.curDir + angle - 360;
-      console.log('newDir',newDir)
+      // console.log('+', 'newDir',newDir)
       return Object.assign({}, currState, {curDir: newDir}, {instr: newInstr});
     case '-':
-          console.log('-')
-
       newDir = currState.curDir - angle >= 0 ? currState.curDir - angle : currState.curDir - angle + 360;
-      console.log('newDir',newDir)
+      // console.log('-', 'newDir',newDir)
       return Object.assign({}, currState, {curDir: newDir}, {instr: newInstr});
     case '[':
       newStack = currState.stack.concat({pos:currState.curPos, dir:currState.curDir});
-      console.log('[', Object.assign({}, currState, {curDir: currState.curDir}, {stack: newStack}, {instr: newInstr}))
+      // console.log('[', Object.assign({}, currState, {curDir: currState.curDir}, {stack: newStack}, {instr: newInstr}))
       return Object.assign({}, currState, {curDir: currState.curDir}, {stack: newStack}, {instr: newInstr});
     case ']':
       let oldPos = currState.stack[currState.stack.length-1];
       newStack = currState.stack.slice(0, currState.stack.length-1);
-      console.log(']', Object.assign({}, currState, {curDir: oldPos.dir}, {curPos: oldPos.pos}, {stack: newStack}, {instr: newInstr}))
+      // console.log(']', Object.assign({}, currState, {curDir: oldPos.dir}, {curPos: oldPos.pos}, {stack: newStack}, {instr: newInstr}))
       return Object.assign({}, currState, {curDir: oldPos.dir}, {curPos: oldPos.pos}, {stack: newStack}, {instr: newInstr});
     default: return Object.assign({}, currState, {instr: newInstr});
   }
@@ -69,8 +66,9 @@ function parseInstr(ctx, currState, dist=20, angle=25){
 function makeInstr(axiom, replace, iterations){
   for(let i = 0; i<iterations; i++){
     axiom = axiom.reduce(function(built, cmd){
+      // console.log(built, cmd)
       if (replace[cmd]) return built.concat(replace[cmd]);
-      else return built;
+      else return built.concat(cmd);
     }, []);
   }
   console.log('made', axiom)
@@ -79,7 +77,7 @@ function makeInstr(axiom, replace, iterations){
 
 //returns start and end pts for draw
 function genLine(curX, curY, dirAngle=35, dist=20){
-    console.log('lala', curX, curY, dirAngle, dist)
+    // console.log('lala', curX, curY, dirAngle, dist)
 
   //(x+c)2 + (y2 + dirAngle*c)2 = dist2
   let rad = dirAngle * Math.PI/180;
@@ -97,7 +95,7 @@ window.addEventListener('resize', function(e){
 
 // let data = {axiom: ['+','+','F','+','F'], replace:{'F': ['F', '+', '+', 'F', 'F', '-', 'F', '+', 'F', '+', 'F', 'F','+']}}
 // let data = {axiom: ['+','+','+','+','+','F'], replace:{'F': ['F','F','-','[','-','F','+','F','+','F',']','+','[','+','F','-','F','-','F',']']}}
-// let data = {axiom: ['F'], replace:{'F': ['F','-','F','+','+','F','-','F']}}
+// let data = {axiom: ['F','+','+', 'F','+','+','F',], replace:{'F': ['F','-','F','+','+','F','-','F']}}
 //should be tree
 // let data = {axiom: ['X'], replace:{'F': ['F','F'], 'X': ['F','-','[','[','X',']','+','X',']','+','F','[','+','F','X',']','-','X']}}
 
@@ -106,16 +104,18 @@ window.addEventListener('resize', function(e){
 
 // let data = {axiom: ['F','X'], replace:{'F': ['X','+','Y','F','+'], Y: ['-','F','X','-','Y']}, angle:90}
 
-let data = {axiom: ['-','F'], replace:{'F': ['F','+','F','+','F','+','F','+','F']}, angle:90}
+// let data = {axiom: ['-','F'], replace:{'F': ['F','+','F','-','F','-','F','+','F']}, angle:90}
+let data = {axiom: ['F','-','X'], replace:{'F': ['F','F'], 'X': ['F','-','[','[','X',']','+','X',']','[','-','X','+',']','F','[','X',']','-','X'], angle:25, dist:2, startPos:[500,0]}}
+
 
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   let canvas = document.getElementById('canvas');
   let ctx = init(canvas, data);
 
-  let explodedInstr = makeInstr(data.axiom, data.replace, data.iterations || 4);
+  let explodedInstr = makeInstr(data.axiom, data.replace, data.iterations || 5);
 
-  let currState = {curPos: data.startPos || [300,300], stack: [], curDir: data.startDir || 0, instr: explodedInstr};
+  let currState = {curPos: data.startPos || [0,700], stack: [], curDir: data.startDir || 0, instr: explodedInstr};
 
   while(currState.instr.length){
     currState = parseInstr(ctx, currState, data.dist || 20, data.angle);
@@ -133,7 +133,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 function drawLine(ctx, startX, startY, endX, endY, color){
-  console.log('hi', startX, startY, endX, endY)
+  // console.log('hi', startX, startY, endX, endY)
   if (!color, color = 'blue');
   ctx.beginPath();
   ctx.strokeStyle = color;
